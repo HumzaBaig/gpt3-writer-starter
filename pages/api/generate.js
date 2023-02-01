8a3ad3349648c4b22e85813c085c1b417c475ed7
HumzaBaig <humzacodemail@gmail.com>
2023-02-01 04:42:59 -0600

    rewrite generate.js file so that the fetch method doesn't time out in less than 15 seconds. This is a trial to see if the production application will wait long enough for OpenAI to respond

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,52 +1,57 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-});
-
-const openai = new OpenAIApi(configuration);
-
-const basePromptPrefix =
-`
-Write either a very impactful reference to a Sahih (authentic) Hadith of the Prophet Muhammad ﷺ, a quote from a famous Muslim scholar, or a verse from the Holy Quran about the topic below. Make sure to include exact location and details of the reference.
-
-Topic:
-`;
-
 const generateAction = async (req, res) => {
     // Run first prompt
 
-    const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-        temperature: 0.8,
-        max_tokens: 256
+    const baseCompletion = await fetch('https://api.openai.com/v1/engines/text-davinci-003/jobs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+            temperature: 0.8,
+            max_tokens: 256
+        }),
+        timeout: 5000
+    })
+    .then(response => response.json())
+    .then(data => {
+        const basePromptOutput = data.choices.pop();
+        // Create and run second prompt
+        const secondPrompt =
+        `
+        Write a short inspirational and conversational paragraph about the topic and quote below in the style of Imam Omar Suleiman. Make sure not to reference any Wahhabi scholars. This paragraph should also contain a call-to-action. Please make sure the paragraph goes in-depth on the topic and shows that the writer did their research. Use modern emojis to grab the attention of the reader. Also, make sure this paragraph is written by OYD and directly addresses the reader.
+
+        Topic: ${req.body.userInput}
+
+        Quote: ${basePromptOutput.text}
+
+        Paragraph:
+        `;
+
+        return fetch('https://api.openai.com/v1/engines/text-davinci-003/jobs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt: `${secondPrompt}`,
+                temperature: 0.9,
+                max_tokens: 850
+            }),
+            timeout: 15000
+        })
+        .then(response => response.json());
+    })
+    .then(data => {
+        const secondPromptOutput = data.choices.pop();
+        res.status(200).json({ output: secondPromptOutput });
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).json({ error });
     });
-
-    const basePromptOutput = baseCompletion.data.choices.pop();
-
-    // Create and run second prompt
-    const secondPrompt =
-    `
-    Write a short inspirational and conversational paragraph about the topic and quote below in the style of Imam Omar Suleiman. Make sure not to reference any Wahhabi scholars. This paragraph should also contain a call-to-action. Please make sure the paragraph goes in-depth on the topic and shows that the writer did their research. Use modern emojis to grab the attention of the reader. Also, make sure this paragraph is written by OYD and directly addresses the reader.
-
-    Topic: ${req.body.userInput}
-
-    Quote: ${basePromptOutput.text}
-
-    Paragraph:
-    `;
-
-    const secondPromptCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${secondPrompt}`,
-        temperature: 0.9,
-        max_tokens: 850
-    });
-
-    const secondPromptOutput = secondPromptCompletion.data.choices.pop();
-
-    res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
